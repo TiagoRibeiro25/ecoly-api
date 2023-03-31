4ba0c536eff8a22c1838532cdfdf5d8122f86288
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-03-31 15:56:28 +0100

    Added main route files
    users, subscribe, schools, news, meetings and activities

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const usersRouter = require("./routes/users.routes");
+const subscribeRouter = require("./routes/subscribe.routes");
+const schoolsRouter = require("./routes/schools.routes");
+const newsRouter = require("./routes/news.routes");
+const meetingsRouter = require("./routes/meetings.routes");
+const activitiesRouter = require("./routes/activities.routes");
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 3000;
@@ -9,6 +15,17 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/api", (req, res) => {
+	res.json({ message: "Welcome to the Ecoly API" });
+});
+
+app.all("/api/users", usersRouter);
+app.all("/api/subscribe", subscribeRouter);
+app.all("/api/schools", schoolsRouter);
+app.all("/api/news", newsRouter);
+app.all("/api/meetings", meetingsRouter);
+app.all("/api/activities", activitiesRouter);
 
 app.all("*", (req, res) => {
 	res.status(404).json({ message: "Invalid route" });
