222fc42b8e9c11251cd1e33fc1ae15c1c7fc230a
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-05-01 00:32:53 +0100

    add all query validations when adding activity

--- routes/activities.routes.js
+++ routes/activities.routes.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 const activitiesController = require("../controllers/activities.controller");
 
+// POST /api/activities => add an activity / report / theme
 router.post("/", (req, res) => {
+	// if there is no query
+	if (Object.keys(req.query).length === 0) {
+		console.log(Object.keys(req.query).length);
+		return res.status(400).json({
+			error: `you must provide a query fields`,
+		});
+	}
+
 	// to catch invalid query
 	const invalidQuery = Object.keys(req.query).find((key) => key !== "fields");
 	if (invalidQuery) {
@@ -18,12 +27,20 @@ router.post("/", (req, res) => {
 		});
 	}
 
-	if (req.query.fields !== "activities") {
+	// all possible values for the query fields
+	const possibleValues = ["activities", "report", "theme"];
+
+	// if the query is not in the possible values
+	if (!possibleValues.includes(req.query.fields)) {
 		return res.status(400).json({
 			error: `invalid value for query fields`,
 		});
-	} else {
-		return activitiesController.createActivity(req, res);
+	} else if (req.query.fields === "activities") {
+		return activitiesController.addActivity(req, res);
+	} else if (req.query.fields === "report") {
+		// return activitiesController.addReport(req, res);
+	} else if (req.query.fields === "theme") {
+		// return activitiesController.addTheme(req, res);
 	}
 });
 
