919a592ebcb19e47030831cf24a968ec59fbf286
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-05-01 19:25:08 +0100

    convert to base64 image when add activity controller

--- controllers/activities.controller.js
+++ controllers/activities.controller.js
@@ -10,7 +10,6 @@ const activity_images = db.activity_image;
 // TODO => read auth_key for authentication => checking each user is creating the activity (creator_id, school_id)
 exports.addActivity = async (req, res) => {
 	try {
-
 		const activity = await Activities.create(req.body);
 
 		// add blocks of code for the authentication validations
@@ -19,13 +18,16 @@ exports.addActivity = async (req, res) => {
 
 		// if the body includes images add to the activity_images
 		if (req.body.images) {
-			const images = req.body.images;
-			images.forEach(async (image) => {
-				await activity_images.create({
+			const images = req.body.images; // array of images
+			// convert the images to base64
+			const base64Images = images.map((image) => Buffer.from(image, "base64"));
+			// add the images to the activity_images table
+			await activity_images.bulkCreate(
+				base64Images.map((image) => ({
 					activity_id: activity.id,
 					img: image,
-				});
-			});
+				}))
+			);
 
 			return res.status(201).json({
 				message: "Activity added",
@@ -33,21 +35,52 @@ exports.addActivity = async (req, res) => {
 		}
 	} catch (err) {
 		if (err instanceof ValidationError) {
-				if(Object.keys(req.body).length === 0){
-					return res.status(400).json({
-						error: "body cannot be empty",
-					});
-				}
-				// else check if the body includes the required fields otherwise return null errors
-				else{
-					return res.status(400).json({
-						// for each error in the errors array return the error message
-						error: err.errors.map((error) => error.message),
-
-					});
-				}
+			if (Object.keys(req.body).length === 0) {
+				return res.status(400).json({
+					error: "body cannot be empty",
+				});
+			}
+			// else check if the body includes the required fields otherwise return null errors
+			else {
+				return res.status(400).json({
+					// for each error in the errors array return the error message
+					error: err.errors.map((error) => error.message),
+				});
+			}
 		}
 
+		return res.status(500).json({
+			error: err.message || "Something went wrong. Please try again later.",
+		});
+	}
+};
+
+exports.deleteActivity = async (req, res) => {
+	try {
+		const { id } = req.params;
+		// remove the images of the activity also when removing the activity
+		await activity_images.destroy({
+			where: {
+				activity_id: id,
+			},
+		});
+
+		const activity = await Activities.destroy({
+			where: {
+				id: id,
+			},
+		});
+
+		if (activity === 0) {
+			return res.status(404).json({
+				error: "Activity not found",
+			});
+		}
+
+		return res.status(200).json({
+			message: "Activity deleted",
+		});
+	} catch (err) {
 		return res.status(500).json({
 			error: err.message || "Something went wrong. Please try again later.",
 		});
