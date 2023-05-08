b68c920925aeda59e6de144b29975af6de8377ea
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-08 14:29:18 +0100

    Added function to check if env vars exist

---
+++ config/checkEnvs.js
@@ -0,0 +1,47 @@
+const colors = require("colors");
+
+/** @returns {boolean} true if all envs are defined, false if not */
+function checkEnvs() {
+	if (!process.env.JWT_SECRET) {
+		console.log(colors.red("-> ") + colors.cyan("JWT_SECRET ") + colors.red("is not defined!"));
+		return false;
+	}
+
+	if (!process.env.DB_HOST) {
+		console.log(colors.red("-> ") + colors.cyan("DB_HOST ") + colors.red("is not defined!"));
+		return false;
+	}
+
+	if (!process.env.DB_USER) {
+		console.log(colors.red("-> ") + colors.cyan("DB_USER ") + colors.red("is not defined!"));
+		return false;
+	}
+
+	if (!process.env.DB_PASSWORD) {
+		console.log(colors.red("-> ") + colors.cyan("DB_PASSWORD ") + colors.red("is not defined!"));
+		return false;
+	}
+
+	if (!process.env.DB_NAME) {
+		console.log(colors.red("-> ") + colors.cyan("DB_NAME ") + colors.red("is not defined!"));
+		return false;
+	}
+
+	if (!process.env.SEND_EMAIL_AUTH_KEY) {
+		console.log(
+			colors.red("-> ") + colors.cyan("SEND_EMAIL_AUTH_KEY ") + colors.red("is not defined!")
+		);
+		return false;
+	}
+
+	if (!process.env.SEND_EMAIL_URI) {
+		console.log(
+			colors.red("-> ") + colors.cyan("SEND_EMAIL_URI ") + colors.red("is not defined!")
+		);
+		return false;
+	}
+
+	return true;
+}
+
+module.exports = checkEnvs;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const colors = require("colors");
+const checkEnvs = require("./config/checkEnvs");
 const app = require("./app");
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 3000;
 
+if (!checkEnvs()) process.exit(1);
+
 app.listen(PORT, HOST, async () => {
-	// await require("./data/resetDB")(true); // Reset the database
+	// await require("./data/resetDB")(); // Reset the database
 	console.log(
 		colors.green("-> ") +
 			colors.cyan("Server is running on ") +
