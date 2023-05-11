63611f8997a0cea26d06e8f3a24d3ed8436a1d8b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-11 21:49:09 +0100

    Update users.controller.js
    Fixed update highlight badge not working

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -473,16 +473,22 @@ exports.editUserInfo = async (req, res) => {
 
 		// highlighted badge
 		if (req.body.highlightBadgeId) {
-			const userBadge = await UserBadge.findOne({
+			// find the badge that is currently highlighted (if any)
+			const highlightedBadge = await UserBadge.findOne({
+				where: { user_id: id, is_highlight: true },
+			});
+
+			// if there is a highlighted badge, remove the highlight
+			if (highlightedBadge) await highlightedBadge.update({ is_highlight: false });
+
+			// find the badge that the user wants to highlight
+			const badge = await UserBadge.findOne({
 				where: { user_id: id, badge_id: req.body.highlightBadgeId },
 			});
-			if (!userBadge) throw new Error("user_badge_not_found");
+			if (!badge) throw new Error("user_badge_not_found");
 
-			// remove highlight from all other badges
-			await UserBadge.update(
-				{ is_highlight: false },
-				{ where: { user_id: id, badge_id: { [Op.ne]: req.body.highlightBadgeId } } }
-			);
+			// update the highlighted badge
+			await badge.update({ is_highlight: true });
 		}
 
 		// update the user
