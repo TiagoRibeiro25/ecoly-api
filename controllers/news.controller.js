4a49894ef1c7198c7f5e837e807487849a248447
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2023-05-11 22:33:26 +0100

    done

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models/db");
 const News = db.news;
+const sendNewsLetter = require("../utils/sendNewsLetter");
 
 exports.getNews = async (req, res) => {
 	try {
@@ -24,7 +25,7 @@ exports.getSingleNew = async (req, res) => {
 		if (!singleNew) {
 			return res.status(404).json({
 				success: false,
-				message: "School does not exist",
+				message: "New does not exist",
 			});
 		}
 
@@ -38,6 +39,72 @@ exports.getSingleNew = async (req, res) => {
 		return res.status(500).json({
 			success: false,
 			message: "Failed to fetch New",
+		});
+	}
+};
+
+exports.deleteNew = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const newToDelete = await News.findByPk(id);
+
+		if (!newToDelete) {
+			res.status(404).json({
+				success: false,
+				message: "New was not found",
+			});
+		} else {
+			await newToDelete.destroy();
+
+			res.status(200).json({
+				success: true,
+				message: "The new was deleted",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Failed to delete new",
+		});
+	}
+};
+
+exports.addNew = async (req, res) => {
+	const { newToCreate } = req.body;
+
+	try {
+		const existingNew = await News.findOne({ where: { title: newToCreate.title } });
+
+		if (existingNew) {
+			res.status(409).json({
+				success: false,
+				message: "The new already exists",
+			});
+		} else {
+			const newNew = await News.create({
+				title: newToCreate.title,
+				content: newToCreate.content,
+				date_created: newToCreate.date_created,
+				creator_id: newToCreate.creator_id,
+			});
+
+			await sendNewsLetter({
+				title: `${newToCreate.title}`,
+				author: { id: `${newToCreate.creator_id}`, name: `José Nogueira` },
+				content: `${newToCreate.content}`,
+				img: "https://picsum.photos/400/300",
+			});
+
+			res.status(201).json({
+				success: true,
+				message: "New was successfully added",
+			});
+		}
+	} catch (error) {
+		res.status(500).send({
+			success: false,
+			message: "Failed to add the new" + " " + error,
 		});
 	}
 };
--- routes/schools.routes.js
+++ routes/schools.routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const schoolsController = require("../controllers/schools.controller.js");
 const authController = require("../controllers/auth.controller");
 
-router.route("/").get(schoolsController.getSchools).post(schoolsController.addSchool);
+router
+	.route("/")
+	.get(schoolsController.getSchools)
+	.post(authController.verifyToken, authController.verifyIsAdmin, schoolsController.addSchool);
 
 router
 	.route("/:id")
-	.get(authController.verifyToken, authController.verifyIsAdmin, schoolsController.getSchool)
+	.get(schoolsController.getSchool)
 	.delete(
 		authController.verifyToken,
 		authController.verifyIsAdmin,
