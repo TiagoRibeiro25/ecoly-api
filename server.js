5a901cdb210dec66d305b28e91a2ea350416c3c9
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-11 22:56:58 +0100

    update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-if (process.env.NODE_ENV === "development") require("dotenv").config();
+if (process.env.NODE_ENV !== "production" || process.env.NODE_ENV !== "prod") {
+	require("dotenv").config();
+}
+
 const colors = require("colors");
 const checkEnvs = require("./config/checkEnvs");
 const app = require("./app");
 
-const HOST = process.env.HOST || process.env.CYCLIC_BUCKET_NAME || "127.0.0.1";
+const HOST = process.env.HOST;
 const PORT = process.env.PORT || 3000;
 
 if (!checkEnvs()) process.exit(1);
