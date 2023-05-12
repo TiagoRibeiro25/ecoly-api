eb20a80c4c04f80b62a93220056ca6119b46fc9e
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-12 15:57:01 +0100

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const colors = require("colors");
 const checkEnvs = require("./config/checkEnvs");
 const app = require("./app");
 
-const HOST = process.env.HOST;
+const HOST = process.env.HOST || RENDER_EXTERNAL_HOSTNAME || "localhost";
 const PORT = process.env.PORT || 3000;
 
 if (!checkEnvs()) process.exit(1);
