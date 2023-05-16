0e9e38cb2e0cff2abbc449699880e8567bdf6dca
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-16 08:59:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-if (process.env.NODE_ENV !== "production" || process.env.NODE_ENV !== "prod") {
-	require("dotenv").config();
-}
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const colors = require("colors");
 const checkEnvs = require("./config/checkEnvs");
 const app = require("./app");
 
-const HOST = process.env.HOST;
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 
 if (!checkEnvs()) process.exit(1);
