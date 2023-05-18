84532739dbf6a0bcda8a46bc591dbb56de608651
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-18 17:12:37 +0100

    Addex max request size

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const activitiesRouter = require("./routes/activities.routes");
 const app = express();
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "15mb" }));
+
 if (process.env.ENABLE_LOG === "true") app.use(morgan("dev"));
 
 app.get("/api", (_req, res) => {
