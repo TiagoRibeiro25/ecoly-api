8c6aae03083a692ad729cf3a55a6f52070f95f15
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-05-18 23:45:44 +0100

    created the meetings routes

--- routes/meetings.routes.js
+++ routes/meetings.routes.js
@@ -1,4 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const meetingsController = require("../controllers/meetings.controller");
+const meetingsValidator = require("../validators/meetings.validator");
+const authController = require("../controllers/auth.controller");
+
+router.get("/:id", meetingsValidator.validateQueries, (req, res) => {
+	if (req.query.fields === "ata") {
+		return authController.verifyToken(req, res, () => {
+			//next
+			authController.verifyIsVerified(req, res, () => {
+				//next
+				meetingsController.getAtaMeeting(req, res);
+			});
+		});
+	}
+});
+
+router.post(
+	"/",
+	meetingsValidator.validateQueries,
+	authController.verifyToken,
+	authController.verifyIsVerified,
+	meetingsController.createMeeting
+);
+
+router.patch("/:id", meetingsValidator.validateQueries, (req, res) => {
+	if (req.query.fields === "ata") {
+		return authController.verifyToken(req, res, () => {
+			//next
+			authController.verifyIsVerified(req, res, () => {
+				//next
+				meetingsController.addAta(req, res);
+			});
+		});
+	}
+});
+
+router.delete(
+	"/:id",
+	authController.verifyToken,
+	authController.verifyIsAdmin,
+	meetingsController.deleteMeeting
+);
 
 module.exports = router;
