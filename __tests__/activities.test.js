52814abe02f47285707e208d6bf65dac8b900437
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-05-23 22:58:55 +0100

    created the file for unit test on activities

---
+++ __tests__/activities.test.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ path: __dirname + "/../tests.env" });
+const supertest = require("supertest");
+const app = require("../app");
+const db = require("../models/db");
+const resetDB = require("../data/resetDB");
+const getToken = require("../utils/generateTokens");
+let userToken = "";
+let unsignedToken = "";
+
+
