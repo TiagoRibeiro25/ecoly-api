953521b05d91b83ab25a719825b37bb270b063f4
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-06-13 18:22:59 +0100

    style: :art: Formatted non formatted file

--- validators/news.validator.js
+++ validators/news.validator.js
@@ -1,40 +1,48 @@
 const validateImgs = require("../utils/validateImgs");
 
 exports.validateNews = async (req, res, next) => {
+	const { title, content, imgs } = req.body;
 
-    const { title, content, imgs } = req.body;
-    
-    if (!title) {return res.status(400).send({
-        success: false,
-        message: "title of the new not given"
-    })}
+	if (!title) {
+		return res.status(400).send({
+			success: false,
+			message: "title of the new not given",
+		});
+	}
 
-    if (typeof title != "string") {return res.status(400).send({
-        success: false,
-        message: "invalid title"
-    })}
+	if (typeof title !== "string") {
+		return res.status(400).send({
+			success: false,
+			message: "invalid title",
+		});
+	}
 
-    if (!content) {return res.status(400).send({
-        success: false,
-        message: "content of the new not given"
-    })}
+	if (!content) {
+		return res.status(400).send({
+			success: false,
+			message: "content of the new not given",
+		});
+	}
 
-    if (typeof content != "string") {return res.status(400).send({
-        success: false,
-        message: "invalid content"
-    })}
+	if (typeof content !== "string") {
+		return res.status(400).send({
+			success: false,
+			message: "invalid content",
+		});
+	}
 
-    if (!imgs || imgs.length == 0) {return res.status(400).send({
-        success: false,
-        message: "images of the new not given"
-    })}
-    
+	if (!imgs || imgs.length === 0) {
+		return res.status(400).send({
+			success: false,
+			message: "images of the new not given",
+		});
+	}
 
-    if (validateImgs(imgs)) {
+	if (validateImgs(imgs)) {
 		return res.status(400).json({
 			success: false,
 			error: "images must be a valid base64 string",
 		});
-    }
-    next()
-}
+	}
+	next();
+};
